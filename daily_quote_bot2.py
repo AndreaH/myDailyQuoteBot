f93cc1a560394a15ddb9c55d8207e1b536c30e45
James. LH <bbangzip2@gmail.com>
2026-04-01 16:24:52 +0900

    Update daily_quote_bot2.py

--- daily_quote_bot2.py
+++ daily_quote_bot2.py
@@ -10,10 +10,9 @@ from PIL import Image, ImageDraw, ImageFont
 
 # 1. 환경 설정 (환경 변수 권장)
 GENAI_API_KEY = os.getenv("GEMINI_API_KEY")
-#TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN2")
-TELEGRAM_TOKEN = "8716356961:AAGYmW2X5QDqnuuzYWwZFadaMs7Bw4AT5e4"
-#CHAT_ID = os.getenv("CHAT_ID2")
-CHAT_ID = "5038473407"
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
 
 BOOKS = {
     "부동산 및 경제경영": [
